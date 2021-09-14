cfc6e7454da6fa71da4ba298b109a7563b77660d
Sergio <sergon149@alu.centroafuera.es>
2021-09-14 12:20:17 +0200

    he añadido la funcion suma

--- src/com/primerproyecto/Main.java
+++ src/com/primerproyecto/Main.java
@@ -5,6 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hola, esta es mi primera línea de código");
         System.out.println("Esta es mi segunda línea de código");
+        funTest();
         System.out.println("Me despido con mi tercera línea de código");
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println(suma(num1,num2));
+
+    }
+
+    private static void funTest(){
+        System.out.println("Blablabla");
+    }
+
+    public static int suma(int num1, int num2){
+        return num1+num2;
     }
 }
